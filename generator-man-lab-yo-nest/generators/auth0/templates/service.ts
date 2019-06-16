ce9afcc910fd62c85215ffa4ac91f5559d894cf2
Adrian Eguez <eadepto@hotmail.com>
2019-06-16 17:58:05 -0500

    Update service.ts

--- generator-man-lab-yo-nest/generators/auth0/templates/service.ts
+++ generator-man-lab-yo-nest/generators/auth0/templates/service.ts
@@ -1,9 +1,17 @@
 import {Injectable} from '@nestjs/common';
 import {PrincipalAuth0Service} from '@manticore-labs/nest';
+// import {DatosUsuarioService} from "../submodulos-roles-backend/datos-usuario/datos-usuario.service";
+// import {DatosUsuarioCreateDto} from "../submodulos-roles-backend/datos-usuario/datos-usuario-create-dto/datos-usuario-create-dto";
 
 @Injectable()
 export class Auth0Service extends PrincipalAuth0Service {
     constructor() {
         super(AUTH0_CONFIG);
     }
+    
+    /*  NO SE OLVIDE QUE SI VA A USAR LOS DATOS USUSARIO DEBE DE AUMENTAR ESTO
+    constructor(private readonly _datoService: DatosUsuarioService) {
+        super(CONFIG_ENVIRONMENT.auth0, _datoService, DatosUsuarioCreateDto);
+    }
+    */
 }
